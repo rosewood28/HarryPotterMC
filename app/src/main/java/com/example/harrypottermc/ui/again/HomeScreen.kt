9a22c1d4f64da2665ef3cfa76417757dd300ee5e
CatalinPToader <petru.toader0807@stud.acs.upb.ro>
2025-01-07 15:16:51 +0200

    Add scrolling

--- app/src/main/java/com/example/harrypottermc/ui/again/HomeScreen.kt
+++ app/src/main/java/com/example/harrypottermc/ui/again/HomeScreen.kt
@@ -1,16 +1,21 @@
 package com.example.harrypottermc.ui.again
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,6 +32,7 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.harrypottermc.R
@@ -48,13 +54,11 @@ fun HomeScreen(
     homeViewModel: HomeViewModel = viewModel(factory = AppViewModelProvider.Factory),
     modifier: Modifier = Modifier,
 ) {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
     // Observe the StateFlow from the ViewModel
     val homeUiState by homeViewModel.homeUiState.collectAsState()
 
     Scaffold(
-        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
 //        topBar = {
 //            InventoryTopAppBar(
 //                title = stringResource(HomeDestination.titleRes),
@@ -66,7 +70,7 @@ fun HomeScreen(
         HomeBody(
             itemList = homeUiState.itemList,//listOf(),
             onItemClick = navigateToItemDetails,
-            modifier = modifier.fillMaxSize(),
+            modifier = modifier.fillMaxSize().verticalScroll(rememberScrollState()).height(50.dp),
             contentPadding = innerPadding,
         )
     }
@@ -79,6 +83,8 @@ private fun HomeBody(
     modifier: Modifier = Modifier,
     contentPadding: PaddingValues = PaddingValues(0.dp),
 ) {
+    val scrollState = rememberScrollState()
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier,
